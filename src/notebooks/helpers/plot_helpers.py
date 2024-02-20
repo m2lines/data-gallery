8394cdb0bcabb215234487665313efb36274b8b9
suryadheeshjith <Surya.dheeshjith@gmail.com>
2024-02-19 19:04:56 -0500

    Animation notebook complete

--- src/notebooks/helpers/plot_helpers.py
+++ src/notebooks/helpers/plot_helpers.py
@@ -6,43 +6,35 @@ from PIL import Image
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import cmocean
 import imageio
+from matplotlib.animation import FuncAnimation, PillowWriter
 
 
-def create_animation(
-    fun, idx, filename="my-animation.gif", dpi=200, FPS=18, loop=0, deezering=True
-):
-    """
-    See https://pythonprogramming.altervista.org/png-to-gif/
-    fun(i) - a function creating one snapshot, has only one input:
-        - number of frame i
-    idx - range of frames, i in idx
-    FPS - frames per second
-    filename - animation name
-    dpi - set 300 or so to increase quality
-    loop - number of repeats of the gif
-    """
-    frames = []
-    for i in idx:
-        fun(i)
-        plt.savefig(".frame.png", dpi=dpi, bbox_inches="tight")
-        plt.close()
-        if deezering:
-            frames.append(Image.open(".frame.png").convert("RGB"))
-        else:
-            frames.append(Image.open(".frame.png"))
-        print(f"Frame {i} is created", end="\r")
-    os.system("rm .frame.png")
-    # How long to persist one frame in milliseconds to have a desired FPS
-    duration = 1000 / FPS
-    print(f"Animation at FPS={FPS} will last for {len(idx)/FPS} seconds")
-    frames[0].save(
-        filename,
-        format="GIF",
-        append_images=frames[1:],
-        save_all=True,
-        duration=duration,
-        loop=loop,
+def update_frame(i, plot_func, fig, len_idx):
+    # Clear the current figure to prepare for the next frame's plot.
+    plt.clf()
+    plot_func(i, fig, len_idx)
+
+
+def create_gif(plot_func, idx, filename="my-animation.gif", dpi=200, FPS=18, loop=0):
+    fig = plt.figure(figsize=(10, 20))
+    ani = FuncAnimation(
+        fig,
+        update_frame,
+        frames=idx,
+        fargs=(plot_func, fig, len(idx)),
+        repeat=bool(loop),
+        blit=False,
     )
+
+    writer = PillowWriter(fps=FPS, metadata=dict(artist="Me"))
+    ani.save(filename, writer=writer)
+    plt.close(fig)
+
+
+def display_gif(fn):
+    from IPython import display
+
+    return display.HTML('<img src="{}">'.format(fn))
 
 
 def merge_gifs(gif_files, output_file, fps=20):
