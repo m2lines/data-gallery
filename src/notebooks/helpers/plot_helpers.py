97d3b9c26ea3b0411ca2a04832cef7c533c0c2cf
suryadheeshjith <Surya.dheeshjith@gmail.com>
2024-03-20 19:18:36 -0400

    Moved code to notebook

--- src/notebooks/helpers/plot_helpers.py
+++ src/notebooks/helpers/plot_helpers.py
@@ -6,29 +6,6 @@ from PIL import Image
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import cmocean
 import imageio
-from matplotlib.animation import FuncAnimation, PillowWriter
-
-
-def update_frame(i, plot_func, fig, len_idx):
-    # Clear the current figure to prepare for the next frame's plot.
-    plt.clf()
-    plot_func(i, fig, len_idx)
-
-
-def create_gif(plot_func, idx, filename="my-animation.gif", dpi=200, FPS=18, loop=0):
-    fig = plt.figure(figsize=(10, 20))
-    ani = FuncAnimation(
-        fig,
-        update_frame,
-        frames=idx,
-        fargs=(plot_func, fig, len(idx)),
-        repeat=bool(loop),
-        blit=False,
-    )
-
-    writer = PillowWriter(fps=FPS, metadata=dict(artist="Me"))
-    ani.save(filename, writer=writer)
-    plt.close(fig)
 
 
 def display_gif(fn):
